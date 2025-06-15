e90695c151295852fe00cc34215458e1181ff26f
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:30:23 -0300

    ex 2

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var inputNum1 = document.querySelector ("#inputNum1")
+var numeroPessoas = document.querySelector ("#numeroPessoas")
 var btCalcular = document.querySelector ("#btCalcular")
 var h3Resultado = document.querySelector ("#h3Resultado")
 
-function calcularDolar() {
-    var valor1 = Number (inputNum1.value)
+function quantasPessoas() {
+    var pessoas = Number (numeroPessoas.value)
+
+    if (pessoas <= 0){
+        h3Resultado.textContent = "Resultado inválido"
+    }
 
 
-    var dolar = valor1 * 5.57
-    h3Resultado.textContent = "O valor do dolar atual, está: " + dolar.toFixed(2);
+    var numOvos = pessoas * 2
+    var numQueijo = pessoas * 50
+    h3Resultado.textContent = `para ${pessoas} pessoas, você precisará de ${numOvos} ovos e ${numQueijo} gramas de queijo.`
 }
 
 btCalcular.onclick = function() {
-    calcularDolar()
+    quantasPessoas()
 }
